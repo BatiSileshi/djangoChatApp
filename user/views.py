eb8e9ce508e8ae808e9d877e681b7ca04a679a40
jiosaa <batisileshi11@gmail.com>
2022-10-06 02:43:53 -0400

    user app

--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+
+def home(request):
+
+    context={}
+    return render(request, 'user/home.html', context)
